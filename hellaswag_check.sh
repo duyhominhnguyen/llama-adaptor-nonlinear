06a9f93584fe271fee94417a4e7b9b37b07ec287
Minami Kotori <30365921+mtntruong@users.noreply.github.com>
2025-07-14 14:32:30 +0900

    Revise bash scripts

--- hellaswag_check.sh
+++ hellaswag_check.sh
@@ -1,4 +1,4 @@
-python hellaswag_check.py \
+torchrun --nnodes=1 --nproc_per_node=1 --master_port=25010 hellaswag_check.py \
      --ckpt_dir ./alpaca_finetuning_v1/LLaMA-7B \
      --adapter_path ./example_weight/non_linear_prompt_7B.pth \
      --typ_act hypermodel \
